de7d19d8a487b2452810a8390078f9e6a9ab580b
Christina Hedges <christina.l.hedges@nasa.gov>
2023-09-25 13:10:39 -0400

    fixing tests

--- tests/test_scene.py
+++ tests/test_scene.py
@@ -13,6 +13,7 @@ def test_simple_vis_scene():
     )
     locations = np.vstack([row.ravel(), column.ravel()]).T
     s = Scene(locations=locations, shape=(2048, 2048), corner=(-1024, -1024))
+    assert (s.X.sum(axis=0) <= 1. + 1e10).all()
     img = s.model(np.ones(s.X.shape[1]))
     fig, ax = plt.subplots()
     ax.imshow(np.log10(img), origin="lower")
