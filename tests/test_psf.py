0dd07ea77cc66550a818d67713a227445a055b4e
benhord <benjhord@gmail.com>
2024-08-20 16:30:03 -0400

    Version number update

--- tests/test_psf.py
+++ tests/test_psf.py
@@ -12,7 +12,7 @@ from pandorapsf.utils import prep_for_add
 
 
 def test_version():
-    assert __version__ == "0.2.11"
+    assert __version__ == "0.2.12"
 
 
 @pytest.mark.skip(reason="outdated functionality")
