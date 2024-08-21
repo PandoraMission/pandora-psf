56be2ec5b13da70bce754c5c448fba0ae912e29d
benhord <benjhord@gmail.com>
2024-08-21 11:50:13 -0400

    More specific out filepath

--- src/pandorapsf/fit_prf.py
+++ src/pandorapsf/fit_prf.py
@@ -3,11 +3,13 @@
 import numpy as np
 
 from pandorapsf.psf import PSF
+from pandorapsf import PACKAGEDIR
+# import package directory?
 
 _, A_fit, sigma_fit = PSF.from_name("visda").calc_prf_maxima()
 
 np.savetxt(
-    "data/prf_fit_params.csv",
+    PACKAGEDIR + "/data/prf_fit_params.csv",
     np.array([A_fit, sigma_fit]),
     delimiter=",",
     fmt="%f",
